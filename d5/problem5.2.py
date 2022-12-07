4f0d996db29178137ba04890b6c73e439ce815bf
TomasTex <tomasteixeira5@gmail.com>
2022-12-07 14:30:01 +0000

    add day 5

---
+++ d5/problem5.2.py
@@ -0,0 +1,19 @@
+def parseInput():
+    with open("input.txt") as inputText:
+        lineCountrer = 0
+        commandInput = []
+        matrix = [['F', 'C', 'J', 'P', 'H', 'T', 'W'], ['G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H'], ['H', 'P', 'T', 'R'], ['Z', 'S', 'N', 'P', 'H', 'T'], [
+            'N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D'], ['P', 'M', 'G', 'F', 'W', 'D', 'Z'], ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'], ['N', 'D', 'S'], ['D', 'Z', 'S', 'F', 'M']]
+        for line in inputText:
+            if lineCountrer >= 10:
+                commandInput.append([int(line.split()[1]), int(
+                    line.split()[3]) - 1, int(line.split()[5]) - 1])
+            lineCountrer += 1
+
+        for quantity, fromWhere, toWhere in commandInput:
+            what = matrix[fromWhere][-quantity:]
+            matrix[toWhere] += what
+            matrix[fromWhere] = matrix[fromWhere][:-quantity]
+
+
+print(parseInput())
